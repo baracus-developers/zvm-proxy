fd41e6e193bfeceb0a5033a2fd85dd7d533565c5
Jan Blunck <jblunck@suse.de>
2010-07-28 10:44:53 +0200

    Add run_command() subroutine
    Signed-off-by: Jan Blunck <jblunck@suse.de>

--- bazvmpower/lib/VmcpWrapper.pm
+++ bazvmpower/lib/VmcpWrapper.pm
@@ -15,13 +15,24 @@ my %operations = (
     'status' => '/sbin/vmcp query %s',
     );
 
+sub run_command
+{
+    local($command) = @_;
+    local(@result);
+
+    $result[0] = `$command`;
+    $result[1] = $?;
+    $result[2] = $!;
+    return @result;
+}
+
 # "Usage: $0 <operation> <guestname>\n";
 # "    e.g. operation = 'status'\n";
 # "         guestname = 'LINUX101'\n";
 sub power
 {
     local($operation, $hostname) = @_;
-    local($command, $result);
+    local($command, $result, $retval, $errval);
 
     die "Invalid argument: '" . join(" ", @_) . "'"
 	unless ((scalar(@_) == 2) && grep(/$operation/,keys(%operations)));
@@ -29,14 +40,15 @@ sub power
     $command = sprintf( $operations{ $operation }, $hostname );
 
     if ( $operation eq "status") {
-	$command .= " 2>&1 1>/dev/null";
-	$result = `$command`;
-	if ($? == -1) {
-	    die "Error running '$command': $!";
-	} elsif ( $? == 0 ) {
+	$command .= " 2>/dev/null";
+	($result, $retval, $errval) = run_command($command);
+	if ($retval == -1) {
+	    die "Error running '$command': $errval";
+	} elsif ( $retval == 0 ) {
 	    return "Online";
-	} elsif ( $? == 256 ) {
-	    $result =~ m/^Error:.*: #(\d+)$/;
+	} elsif ( $retval == 256 ) {
+	    # Normal termination, WEXITSTATUS($?) == 1
+	    $result =~ m/^HCPCQU(\d+)E.*/;
 	    if ( defined($1) && $1 == 45 ) {
 		return "Offline";
 	    } else {
@@ -49,10 +61,10 @@ sub power
 	}
     } else {
 	$command .= " 2>&1";
-	$result = `$command`;
-	if ($? == -1) {
-	    die "Error running '$command': $!";
-	} elsif ($? != 0) {
+	($result, $retval, $errval) = run_command($command);
+	if ($retval == -1) {
+	    die "Error running '$command': $errval";
+	} elsif ($retval != 0) {
 	    chomp($result);
 	    die ($result || "Unknown error");
 	}
